c0c8d5416d72ea5ec07813dde745bdecc6f3ab55
im4kshay <akshaysayre7@gmail.com>
2022-04-12 00:35:33 +0530

    Lets create a simple Linked List of 56, 30 and 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -12,6 +12,13 @@ namespace LinkedList
         {
             //Welcome Message
             Console.WriteLine("=-=-=-=-=-Welcome To The Linked List Program-=-=-=-=-=");
+
+            //Calling Class LinkedList
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
             Console.ReadLine();
         }
     }
