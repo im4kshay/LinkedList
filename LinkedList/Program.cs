34ccb001971140d2cd4f21e61b48b418693342c0
im4kshay <akshaysayre7@gmail.com>
2022-04-12 00:56:57 +0530

    Ability to insert 30 between 56 and 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -15,11 +15,23 @@ namespace LinkedList
 
             //Calling Class LinkedList
             LinkedList linkedList = new LinkedList();
-            linkedList.Append(56);
-            linkedList.Append(30);
-            linkedList.Append(70);
+            linkedList.Add(56);
+            linkedList.Add(70);
 
-            //Display Data of Linked List
+            //Display Data Before Adding into  Linked List
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Display Data Before Adding into  Linked List ::");
+            Console.WriteLine("-----------------------------------------------");
+            linkedList.Display();
+
+            //Adding Data Between 2 Data
+            linkedList.InsertAtParticularPosition(2, 30);
+
+            //Display Data After Adding into  Linked List
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Display Data Before Adding into  Linked List ::");
+            Console.WriteLine("-----------------------------------------------");
+
             linkedList.Display();
 
             Console.ReadLine();
